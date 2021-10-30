9f31f651ab799ea8ce867e6a057b75b6055a98a0
Marcin Grabysz <01158697@pw.edu.pl>
2021-10-30 23:47:43 +0200

    Implemented fitness and reproduction as functions

--- evolutionary_algorithm.py
+++ evolutionary_algorithm.py
@@ -50,3 +50,80 @@ class Algorithm:
         # if individual is correct its fitness is number of different values
         a_set = set(individual)
         return len(a_set)
+
+# ===================================================================================
+
+
+class Individual:
+    def __init__(self, core):
+        self.core = core        # list
+        self.fitness = None     # integer
+
+    def __str__(self):
+        return str(self.core)
+
+
+def initialize_randomly(population_size, individual_size, seed=None):
+    """
+    Returns randomly initiated population
+    """
+    random.seed(seed)
+    population = []
+    max_number = individual_size - 1
+
+    for _ in range(population_size):
+        individual_core = [
+            randint(0, max_number) for _ in range(individual_size)
+            ]
+        individual = Individual(individual_core)
+        population.append(individual)
+
+    return population
+
+
+def fitness(individual, individual_size, edges):
+    """
+    Funtion evaluates given individual.
+    Fitness value equals to number of colors used in graph, therefore
+    the smaller value is the better.
+    Individuals which are not correct (connected nodes have the same
+    color) are evaluated as n+1 where n is number of nodes
+    """
+    # check every pair of connected nodes (every edge)
+    for (u, v) in edges:
+        if individual.core[u] == individual.core[v]:
+            return individual_size + 1
+
+    # if individual is correct its fitness is number of different values
+    a_set = set(individual.core)
+    return len(a_set)
+
+
+def reproduction(
+    population,
+    population_size,
+    individual_size,
+    edges,
+    seed=None
+):
+
+    random.seed(seed)
+    new_population = []
+    for _ in range(population_size):
+        opponent_1 = random.choice(population)
+        opponent_2 = random.choice(population)
+        fitness_1 = fitness(opponent_1, individual_size, edges)
+        fitness_2 = fitness(opponent_2, individual_size, edges)
+
+        if fitness_1 < fitness_2:
+            # opponent_1 is better
+            new_population.append(opponent_1)
+        elif fitness_1 > fitness_2:
+            # opponent 2 is better
+            new_population.append(opponent_2)
+        else:
+            # opponents are equally good
+            winner = random.choice([opponent_1, opponent_2])
+            new_population.append(winner)
+
+    return new_population
--- test_evolutionary_algorithm.py
+++ test_evolutionary_algorithm.py
@@ -1,20 +1,10 @@
 # import pytest
-from evolutionary_algorithm import Algorithm
+from evolutionary_algorithm import (
+    Individual,
+    initialize_randomly,
+    fitness
+)
 from graph import Graph
-
-
-def test_inizialize_randomly():
-    graph = Graph(nodes=5, seed=1)
-    algorithm = Algorithm(graph=graph, population_size=4, seed=1)
-
-    actual_population = algorithm.initialize_randomly()
-    expected_population = [
-        [1, 4, 0, 2, 0],
-        [3, 3, 3, 3, 1],
-        [0, 3, 0, 3, 3],
-        [4, 0, 3, 2, 1]
-        ]
-    assert actual_population == expected_population
 
 
 def test_graph_getters():
@@ -24,13 +14,23 @@ def test_graph_getters():
     assert graph.number_of_nodes() == 5
 
 
+def test_inizialize_randomly():
+    actual_population = initialize_randomly(4, 5, 1)
+    actual_population_cores = [i.core for i in actual_population]
+    expected_population_cores = [
+        [1, 4, 0, 2, 0],
+        [3, 3, 3, 3, 1],
+        [0, 3, 0, 3, 3],
+        [4, 0, 3, 2, 1]
+        ]
+    assert actual_population_cores == expected_population_cores
+
+
 def test_fitness():
-    graph = Graph(nodes=5, seed=1)
-    algorithm = Algorithm(graph=graph, population_size=4, seed=1)
-    # edges = [(2, 4), (3, 4)]
-    individual = [1, 2, 3, 4, 5]           # correct individual
-    better_individual = [1, 1, 1, 4, 5]    # better individual
-    malformed_individual = [1, 2, 3, 4, 3]
-    assert algorithm.fitness(individual) == 5
-    assert algorithm.fitness(better_individual) == 3
-    assert algorithm.fitness(malformed_individual) == 6
+    edges = [(2, 4), (3, 4)]
+    individual = Individual([1, 2, 3, 4, 5])           # correct individual
+    better_individual = Individual([1, 1, 1, 4, 5])
+    malformed_individual = Individual([1, 2, 3, 4, 3])
+    assert fitness(individual, 5, edges) == 5
+    assert fitness(better_individual, 5, edges) == 3
+    assert fitness(malformed_individual, 5, edges) == 6
